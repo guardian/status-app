a3fb1190dfba88715e6118cba021c5fdce5dcc0d
lindseydew <lindsey.dew@guardian.co.uk>
2016-05-04 14:46:57 +0100

    update Estate model in test

--- test/model/EstateTest.scala
+++ test/model/EstateTest.scala
@@ -40,7 +40,7 @@ class EstateTest extends Specification {
   }
 
   def asg(stage: String, stack: Option[String] = None, numMembers: Int = 1) = ASG(
-    s"name-${Random.alphanumeric}", Some(stage), None, stack, None,
-      Seq.fill(numMembers)(ASGMember(Random.alphanumeric.toString(), None, "", None, None, "", "", null)), Nil, Nil, Nil, None, None
+    Some(s"name-${Random.alphanumeric}"), Some(stage), None, stack, None,
+      Seq.fill(numMembers)(ASGMember(Random.alphanumeric.toString(), None, "", None, None, None, "", null)), Nil, Nil, Nil, None, None
   )
 }
