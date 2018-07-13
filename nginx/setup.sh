4b9f80cba95506244a50d6af27947a08552af7fd
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2018-07-13 17:51:08 +0100

    Update directions for running locally and add nginx setup

---
+++ nginx/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+NGINX_HOME=$(nginx -V 2>&1 | grep 'configure arguments:' | sed 's#.*conf-path=\([^ ]*\)/nginx\.conf.*#\1#g')
+
+echo "\n\nUsing NGINX_HOME=$NGINX_HOME"
+
+sudo mkdir -p $NGINX_HOME/sites-enabled
+sudo ln -fs $DIR/status.conf $NGINX_HOME/sites-enabled/status.conf
