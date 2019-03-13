88ed674d2ab1b3db159aa36f2b5408247069cefd
Roberto Tyley <roberto.tyley@theguardian.com>
2019-03-13 16:45:09 +0000

    Pretty sure we should default to serving HTTPS, not insecure HTTP

--- app/lib/Config.scala
+++ app/lib/Config.scala
@@ -19,7 +19,7 @@ class DynamoConfig(mode: Mode, httpConfiguration: HttpConfiguration) {
   lazy val googleAuthConfig: GoogleAuthConfig = {
     val oauthConfig = get("oauth")
     val host = if (mode == Mode.Dev) "status.thegulocal.com" else oauthConfig("host").getS
-    val protocol = if (mode == Mode.Dev) "https" else oauthConfig.get("protocol").map(_.getS).getOrElse("http")
+    val protocol = if (mode == Mode.Dev) "https" else oauthConfig.get("protocol").map(_.getS).getOrElse("https")
 
     val clientId = oauthConfig("clientId").getS
     val clientSecret = oauthConfig("clientSecret").getS
